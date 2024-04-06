88e507d2586fa56180e666f72bcbe2b30dcd0b26
AngelCreates <palakofficial2025@gmail.com>
2024-04-07 05:01:17 +0530

    changes in file Uploads

--- frontend/src/component/uploadNote/UploadNote.js
+++ frontend/src/component/uploadNote/UploadNote.js
@@ -15,6 +15,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { mobile } from "../../responsive";
 // import {Page, Text, Image, Document, StyleSheet} from '@react-pdf/renderer'
 
+
 const useStyles = makeStyles((theme) => ({
   uploadIcon: {
     color: "#8da6ff",
@@ -53,11 +54,19 @@ const UploadNote = () => {
   const { currentUser: user } = useSelector((state) => state.user);
   const pf = "https://notesharingbackend-ankitkr437.onrender.com/images/";
 
+  //
+
+// const handleFileChange = (e) => {
+//   setFile(e.target.files[0]);
+// };
+
+//
+
   const ShowForm = useRef();
   const notename = useRef();
   const descritpion = useRef();
   const [isupload, setsetisupload] = useState(false);
-  const [file, setfile] = useState("");
+  const [file, setfile] = useState(null);
   const [fileimg, setfileimg] = useState(null);
   const ShowFormHandler = () => {
     if ((ShowForm.current.style.display = "flex"))
@@ -65,23 +74,31 @@ const UploadNote = () => {
   };
 
   const uploadNoteFormSubmitHandler = async (e) => {
-    alert("Uploading started, it will take few minutes...");
+
+    alert("Uploading started, it will take few minutes...")
     e.preventDefault();
-    const newNote = {
-      userId: user._id,
-      desc: descritpion.current.value,
-      notename: notename.current.value,
-      notefilename: file,
-    };
-    if (fileimg) {
-      const data = new FormData();
-      data.append("file", fileimg);
-      data.append("upload_preset", "handnoteimages");
-      const res = await axios.post(
-        "https://api.cloudinary.com/v1_1/dw2fok6if/image/upload",
-        data
-      );
-      newNote.thumbnailfilename = await res.data.secure_url;
+   const newNote = {
+     userId: user._id,
+     desc: descritpion.current.value,
+     notename:notename.current.value,
+     notefilename :fileurl,
+   };
+   if (fileimg) {
+     const data = new FormData();
+     data.append("file", fileimg);
+     data.append("upload_preset", 'handnoteimages');
+     const res=await axios.post("https://api.cloudinary.com/v1_1/dw2fok6if/image/upload",data)
+     newNote.thumbnailfilename = await  res.data.secure_url;
+     
+//    
+    // else if (file) {
+    //   const data = new FormData();
+    //   data.append("file", file);
+    //   data.append("upload_preset", "handnotefiles");
+    //   const res = await axios.post(
+    //     "mongodb+srv://sanyogsingrajput:SanyogCodeWizards@notebox-clustor1.sm1d0im.mongodb.net/?retryWrites=true&w=majority&appName=Notebox-clustor1",data
+    //   );
+    //   newNote.thumbnailfilename = await res.data.secure_url;
     }
     try {
       await publicRequest.post("/notes", newNote);
@@ -93,88 +110,67 @@ const UploadNote = () => {
   const classes = useStyles();
   return (
     <>
-      {!isupload && (
-        <div className="upload-container">
-          <div
-            className="uploadNote-post"
-            onClick={() => {
-              setsetisupload((curr) => !curr);
-            }}
-          >
-            {/* <PictureAsPdf className={classes.UploadPdfIcon} /> */}
-            <p className="uploadNote-post-text">Upload Your Note</p>
-            <CloudUpload className={classes.uploadIcon} />
-          </div>
+       {
+       !isupload &&
+        <div className="uploadNote-post"  onClick={() => {setsetisupload((curr) => !curr);}}>
+          <PictureAsPdf  className={classes.UploadPdfIcon}/>
+          <p className="uploadNote-post-text">
+            Upload a Note
+          </p>
+          <CloudUpload className={classes.uploadIcon}/>
         </div>
-      )}
-      {isupload && (
-        <div className="uploadNote-form-active">
-          <div className="uploadNote-form-container">
-            <div className="upload-note-top">
-              <div className="upload-note-top-left">
-                <AddCircle className={classes.uploadAdd} />
-                <p className="upload-note-title">Upload a Note</p>
-              </div>
-              <Close
-                onClick={() => {
-                  setsetisupload((curr) => !curr);
-                }}
-                className={classes.closeIcon}
-                id="close-icon"
-              />
-            </div>
-            <form
-              onSubmit={uploadNoteFormSubmitHandler}
-              className="uploadNote-form"
+        }
+        { isupload && <div className="uploadNote-form-container" >
+          <div className="upload-note-top">
+          <div className="upload-note-top-left">
+          <AddCircle className={classes.uploadAdd}/>
+          <p className="upload-note-title">Upload a Note</p>
+          </div> 
+        <Close onClick={() => {setsetisupload((curr) => !curr);}} className={classes.closeIcon} id="close-icon"/>
+          </div>
+          <form onSubmit={uploadNoteFormSubmitHandler} className="uploadNote-form">
+            <input
+              type="text"
+              placeholder="Notename(not more than 30 character)*"
+              className="uploadNote-form-note-name"
+              id="upload-note-input"
+              ref={notename}
+              maxLength="30"
+              required
+            ></input>
+            <input
+              type="text"
+              placeholder="Descritpion(not more than 300 character)*"
+              className="uploadNote-form-descritpion"
+              ref={descritpion}
+              id="upload-note-input"
+              maxLength="300"
+              required
+            ></input>
+            <input
+              type="text"
+              id="upload-note-input"
+              onChange={(e)=>setfileurl(e.target.value)}
+              placeholder="Url of note*"
+              required
+            ></input>
+            <label for="thumbnail-file-upload" className="custom-file-upload">
+              <Image className={classes.upload}/>
+              <p>Upload a thumbnail image</p>
+             <CloudUpload className={classes.upload}/>
+            </label>
+            <input
+            placeholder="Thumbnail image"
+              type="file"
+              id="thumbnail-file-upload"
+             accept=".png,.jpeg,.jpg"
+             onChange={(e)=>setfileimg(e.target.files[0])}
             >
-              <input
-                type="text"
-                placeholder="Notename(not more than 30 character)*"
-                className="uploadNote-form-note-name"
-                id="upload-note-input"
-                ref={notename}
-                maxLength="30"
-                required
-              ></input>
-              <input
-                type="text"
-                placeholder="Descritpion(not more than 300 character)*"
-                className="uploadNote-form-descritpion"
-                ref={descritpion}
-                id="upload-note-input"
-                maxLength="300"
-                required
-              ></input>
-
-              <input
-                // accept="application/pdf"
-                type="file"
-                id="upload-note-input"
-                accept=".pdf, .txt, .doc, .docx"
-                onChange={(e) => setfile(e.target.files[0])}
-                placeholder="Choose your file"
-                required 
-              ></input>
-
-              <label for="thumbnail-file-upload" className="custom-file-upload">
-                <Image className={classes.upload} />
-                <p>Upload a thumbnail image</p>
-                <CloudUpload className={classes.upload} />
-              </label>
-              <input
-                placeholder="Thumbnail image"
-                type="file"
-                id="thumbnail-file-upload"
-                accept=".png,.jpeg,.jpg"
-                onChange={(e) => setfileimg(e.target.files[0])}
-              ></input>
-              <button type="submit" className="uploadNote-form-submit-button">
-                Upload
-              </button>
-            </form>
-          </div>
+            </input>
+            <button type="submit" className="uploadNote-form-submit-button">Upload</button>
+          </form>
         </div>
-      )}
+        }
     </>
   );
 };
